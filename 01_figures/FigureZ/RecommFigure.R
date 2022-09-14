f0a1e723e238169e87904fd43bb946bb3b58f283
Matt Pinder <matt_pinder13@hotmail.com>
2022-09-14 08:37:19 +0200

    Update colour scheme for Figure Z

--- 01_figures/FigureZ/RecommFigure.R
+++ 01_figures/FigureZ/RecommFigure.R
@@ -7,9 +7,9 @@ infile <- read.csv("RecommendationCategories.tsv", header=T, sep="\t")
 
 svg("Förslag.svg")
 ggplot(infile, aes(x=reorder(Category, NumberOfPapers), y=NumberOfPapers)) +
-#	geom_col(fill="blue") + coord_flip() +
 	geom_col(aes(fill=NumberOfPapers)) + coord_flip() +
 	geom_text(aes(label=NumberOfPapers), nudge_y = 2) +
+	scale_fill_gradient2(mid="yellow", high="red") +
 	labs(x = "Kategori", y="Antal artiklar",
 		title = "[Insert title here]") +
 	background_grid(major = "none", minor = "none") +
@@ -17,11 +17,9 @@ ggplot(infile, aes(x=reorder(Category, NumberOfPapers), y=NumberOfPapers)) +
 	panel_border(colour = "black", size = 2) +
 	theme(plot.title = element_text(hjust = 0.1)) +
 	theme(panel.spacing = unit(0.1, "lines")) +
-	theme(legend.title=element_blank()) +
 	theme(text=(element_text(size=12))) +
 	theme(axis.text=(element_text(size=13, colour = "Black"))) +
 	theme(panel.background = element_blank()) +
-	theme(legend.key = element_blank()) +
-	theme(legend.text = element_text(face = "italic")) +
+	theme(legend.position="none") +
 	theme(aspect.ratio=1)
 dev.off()
